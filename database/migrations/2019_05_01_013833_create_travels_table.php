706b82dbe6d9d4b69c2f924c00a1b4f5a3ef71b1
JodyYan <stu891272004@yahoo.com.tw>
2019-05-01 10:13:26 +0800

    create tables

---
+++ database/migrations/2019_05_01_013833_create_travels_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->integer('id');
+            $table->string('lang');
+            $table->string('name');
+            $table->string('summary')->nullable();
+            $table->string('introduction')->nullable();
+            $table->string('open_time')->nullable();
+            $table->string('district')->nullable();
+            $table->string('address')->nullable();
+            $table->string('tel')->nullable();
+            $table->string('fax')->nullable();
+            $table->string('lat')->nullable();
+            $table->string('long')->nullable();
+            $table->string('services')->nullable();
+            $table->string('catagory')->nullable();
+            $table->string('update_time')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
