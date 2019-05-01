8c522636e8f6381e3a17685c6b002860196f23f9
JodyYan <stu891272004@yahoo.com.tw>
2019-05-01 17:03:59 +0800

    add paginate to function show

--- app/Http/Controllers/TravelController.php
+++ app/Http/Controllers/TravelController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Travel;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Http\Request;
 
 class TravelController extends Controller
 {
     public function show(){
         $address=request()->get('address');
-        $result=Travel::where('address', 'LIKE', '%' . $address . '%')->get();
+        $result=Travel::where('address', 'LIKE', '%' . $address . '%')->paginate(5)->appends(['address' => $address]);
         return $result;
     }
 }
